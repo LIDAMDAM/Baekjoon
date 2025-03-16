f84832d9bea180463530d71414c044b742662a13
LIDAMDAM <phillip3652@gmail.com>
2025-03-17 00:19:13 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/11653. 소인수분해/소인수분해.py
+++ Python/백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,10 +1,16 @@
 import sys
 n = int(input())
 
-for i in range(2, n+1):
-    if n == 0:
-        break
-    elif n%i == 0:
-        while n%i == 0:
-            n /= i
-            sys.stdout.write(f"{str(i)}\n")
+while n % 2 == 0:
+    sys.stdout.write("2\n")
+    n //= 2
+
+i = 3
+while i * i <= n:
+    while n % i == 0:
+        sys.stdout.write(f"{i}\n")
+        n //= i
+    i += 2
+
+if n > 1:
+    sys.stdout.write(f"{n}\n")
