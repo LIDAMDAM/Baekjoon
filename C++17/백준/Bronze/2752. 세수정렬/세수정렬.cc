43d1cd198c9aa4fa07a06cfd5cc846a73eb4e6d3
Daham <phillip3652@gmail.com>
2025-04-17 13:36:03 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> arr(3, 0);
+    for (int i = 0; i<3; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+    
+    for (int i = 0; i<3; i++){
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
