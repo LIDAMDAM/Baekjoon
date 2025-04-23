0ff10cb77ee44f90e20615466df8cd08bb7302c9
Daham <phillip3652@gmail.com>
2025-04-23 15:08:15 +0900

    [Silver V] Title: 숫자 카드, Time: 396 ms, Memory: 122120 KB -BaekjoonHub

--- Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,15 +1,17 @@
 import sys
 
+input = sys.stdin.readline
+
 n = int(input())
-setN = set(map(int, sys.stdin.readline().split()))
+setN = set(map(int, input().split()))
 m = int(input())
-listM = list(map(int, sys.stdin.readline().split()))
-ansNum = set(set(listM) & setN)
-ansArray = list()
+listM = list(map(int, input().split()))
+
+ansArray = []
 
 for i in listM:
-    if i in ansNum:
-        ansArray.append(1)
-    else: ansArray.append(0)
+    if i in setN:
+        ansArray.append("1")
+    else: ansArray.append("0")
 
-print(' '.join(map(str, ansArray)))
+print(' '.join(ansArray))
